bc541732db9c25f395db74e19a4597c867ab9da6
Jens Rydenheim <jens.rydenheim@gmail.com>
2017-11-04 21:01:46 +0100

    Add script for updating teams-data

--- admin.js
+++ admin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./data/motionsserien-serviceAccountKey.json");
+const teamsFile = require("./data/motionsserien-teams.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -7,11 +8,40 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-const divisions = db.ref("divisions");
+const divisionsRef = db.ref("divisions");
+const teamsRef = db.ref("teams");
 
-divisions.once("value", function(snapshot) {
-    let updatedDivisions = addMatchId(snapshot.val())
-    divisions.set(updatedDivisions, function(error) {
+//RUN HERE
+setTeams();
+
+
+function addMatchIdToEachMatch() {
+    divisionsRef.once("value", function(snapshot) {
+        let updatedDivisions = addMatchId(snapshot.val())
+        divisionsRef.set(updatedDivisions, function(error) {
+            if (error) {
+                console.log("Data could not be saved." + error);
+            } else {
+                console.log("Data saved successfully.");
+            }
+            process.exit();
+        });
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        process.exit(1);
+    });
+
+    function addMatchId(divisions) {
+        for (let matchid in divisions.div3.matches) {
+            divisions.div3.matches[matchid]['matchid'] = matchid;
+        }
+        //console.log(JSON.stringify(divisions, null, 2));
+        return divisions;
+    }
+}
+
+function setTeams() {
+    teamsRef.set(teamsFile.teams, function(error) {
         if (error) {
             console.log("Data could not be saved." + error);
         } else {
@@ -19,15 +49,4 @@ divisions.once("value", function(snapshot) {
         }
         process.exit();
     });
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-    process.exit(1);
-});
-
-function addMatchId(divisions) {
-    for (let matchid in divisions.div3.matches) {
-        divisions.div3.matches[matchid]['matchid'] = matchid;
-    }
-    //console.log(JSON.stringify(divisions, null, 2));
-    return divisions;
 }
