f1739e87aa0ce60bc5cfcf2f00f828ea129af4e9
Jens Rydenheim <jens.rydenheim@gmail.com>
2017-11-05 21:35:48 +0100

    Add script for setting matches data

--- admin.js
+++ admin.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./data/motionsserien-serviceAccountKey.json");
 const teamsFile = require("./data/motionsserien-teams.json");
+const matchesFile = require("./data/motionsserien-matches.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -10,22 +11,17 @@ admin.initializeApp({
 const db = admin.database();
 const divisionsRef = db.ref("divisions");
 const teamsRef = db.ref("teams");
+const matchesRef = db.ref("matches");
 
 //RUN HERE
+setMatches();
 setTeams();
 
 
 function addMatchIdToEachMatch() {
     divisionsRef.once("value", function(snapshot) {
         let updatedDivisions = addMatchId(snapshot.val())
-        divisionsRef.set(updatedDivisions, function(error) {
-            if (error) {
-                console.log("Data could not be saved." + error);
-            } else {
-                console.log("Data saved successfully.");
-            }
-            process.exit();
-        });
+        divisionsRef.set(updatedDivisions, catchAnswer);
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
         process.exit(1);
@@ -41,12 +37,18 @@ function addMatchIdToEachMatch() {
 }
 
 function setTeams() {
-    teamsRef.set(teamsFile.teams, function(error) {
-        if (error) {
-            console.log("Data could not be saved." + error);
-        } else {
-            console.log("Data saved successfully.");
-        }
-        process.exit();
-    });
+    teamsRef.set(teamsFile.teams, catchAnswer);
+}
+
+function setMatches() {
+    matchesRef.set(matchesFile.matches, catchAnswer);
+}
+
+function catchAnswer(error) {
+    if (error) {
+        console.log("Data could not be saved." + error);
+    } else {
+        console.log("Data saved successfully.");
+    }
+    process.exit();
 }
