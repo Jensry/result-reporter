f74f602170e3317de7c1849b02b6606dd34780a8
Jens Rydenheim <jens.rydenheim@gmail.com>
2018-03-22 07:45:45 +0100

    Use Buefy instead of Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-require('bootstrap/dist/css/bootstrap.min.css');
+//require('bootstrap/dist/css/bootstrap.min.css');
 
+import 'buefy/lib/buefy.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase'
 import VueFire from 'vuefire'
+import Buefy from 'buefy'
+
 
 Vue.config.productionTip = false
 
 Vue.use(VueFire)
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
