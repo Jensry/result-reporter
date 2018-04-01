dc01c6207fdc69aac39559e934d2cd2eb496088f
Anthon Holmqvist <anthon.holmqvist@softhouse.se>
2018-04-01 22:26:54 +0200

    Add two components that should be used for games
    Commit will add the following components:
    * GameResult
    * GameResultInput
    
    GameResult component will emit an input event when editing any field
    within the component. Input event will emit a game object that has
    three sets, setOne, setTwo and setThree.

---
+++ src/components/GameResult.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column">
+        <game-result-input
+          heading="Set 1"
+          @input="(value) => onInput('setOne', value)"
+        >
+        </game-result-input>
+      </div>
+      <div class="column">
+        <game-result-input
+          heading="Set 2"
+          @input="(value) => onInput('setTwo', value)"
+        >
+        </game-result-input>
+      </div>
+      <div class="column">
+        <game-result-input
+          heading="Set 3"
+          @input="(value) => onInput('setThree', value)"
+        >
+        </game-result-input>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import GameResultInput from "./GameResultInput";
+
+export default {
+  name: "GameResult",
+  data() {
+    return {
+      result: {
+        setOne: {home: '', away: ''},
+        setTwo: {home: '', away: ''},
+        setThree: {home: '', away: ''},
+      },
+    };
+  },
+  methods: {
+    onInput(key, value) {
+      this.result[key] = value;
+      this.$emit('input', this.result);
+    },
+  },
+  components: {GameResultInput}
+}
+</script>
+
+<style lang="scss" scoped>
+  .section {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+</style>
