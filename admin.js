e9e7d7cc1b46e67c30eccd2153172d87df173239
Jens Rydenheim <jens.rydenheim@gmail.com>
2018-12-28 23:25:33 +0100

    Add admin code to populate bkc database

--- admin.js
+++ admin.js
@@ -1,50 +1,67 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./data/motionsserien-serviceAccountKey.json");
-const teamsFile = require("./data/motionsserien-teams.json");
-const matchesFile = require("./data/motionsserien-matches.json");
+const serviceAccount = require("./data/bkc-motionsserien-serviceAccountKey.json");
+const matchesDiv1File = require("./data/bkc-motionsserien-div1-matches-raw.json");
+const teamsDiv1File = require("./data/bkc-motionsserien-div1-teams-raw.json");
+const matchesDiv2File = require("./data/bkc-motionsserien-div2-matches-raw.json");
+const teamsDiv2File = require("./data/bkc-motionsserien-div2-teams-raw.json");
+const matchesDiv3File = require("./data/bkc-motionsserien-div3-matches-raw.json");
+const teamsDiv3File = require("./data/bkc-motionsserien-div3-teams-raw.json");
+const matchesDiv4File = require("./data/bkc-motionsserien-div4-matches-raw.json");
+const teamsDiv4File = require("./data/bkc-motionsserien-div4-teams-raw.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://motionsserien-3a987.firebaseio.com"
+  databaseURL: "https://bkc-motionsserien.firebaseio.com"
 });
 
 const db = admin.database();
 const divisionsRef = db.ref("divisions");
-const teamsRef = db.ref("teams");
-const matchesRef = db.ref("matches");
 
-//RUN HERE
-setMatches();
-setTeams();
+//Setup divisions to store here:
+addDivision("div1", "Div 1", matchesDiv1File, teamsDiv1File);
+addDivision("div2", "Div 2", matchesDiv2File, teamsDiv2File);
+addDivision("div3", "Div 3", matchesDiv3File, teamsDiv3File);
+addDivision("div4", "Div 4", matchesDiv4File, teamsDiv4File);
 
-function addMatchIdToEachMatch() {
-  divisionsRef.once(
-    "value",
-    function(snapshot) {
-      let updatedDivisions = addMatchId(snapshot.val());
-      divisionsRef.set(updatedDivisions, catchAnswer);
-    },
-    function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-      process.exit(1);
+function addDivision(divId, divName, matchesFile, teamsFile) {
+  let division = {
+    divid: divId,
+    divname: divName,
+    matches: getMatches(matchesFile),
+    teams: getTeams(teamsFile)
+  };
+  divisionsRef.child(divId).set(division, catchAnswer);
+}
+
+function getMatches(matchesFile) {
+  let matches = {};
+  for (let matchKey in matchesFile) {
+    const match = matchesFile[matchKey];
+    const matchId = "match" + pad(match["Matchnummer"]);
+    matches[matchId] = {
+      hometeam: toValidKey(match["Hemmalag"]),
+      awayteam: toValidKey(match["Bortalag"]),
+      court: match["Bana"],
+      date: match["Datum"],
+      matchid: matchId,
+      matchnumber: match["Matchnummer"],
+      result: match["Resultat"],
+      round: match["Spelomgång"],
     }
-  );
-
-  function addMatchId(divisions) {
-    for (let matchid in divisions.div3.matches) {
-      divisions.div3.matches[matchid]["matchid"] = matchid;
-    }
-    //console.log(JSON.stringify(divisions, null, 2));
-    return divisions;
   }
+  return matches;
 }
 
-function setTeams() {
-  teamsRef.set(teamsFile.teams, catchAnswer);
-}
-
-function setMatches() {
-  matchesRef.set(matchesFile.matches, catchAnswer);
+function getTeams(teamsFile) {
+  let teams = {};
+  for (let teamKey in teamsFile) {
+    const team = teamsFile[teamKey]
+    teams[toValidKey(teamKey)] = {
+      name: team["Team"],
+      members: [team["Member1"], team["Member2"], team["Member3"], team["Member4"], team["Member5"]].filter(name => name), 
+    }
+  }
+  return teams;
 }
 
 function catchAnswer(error) {
@@ -54,4 +71,14 @@ function catchAnswer(error) {
     console.log("Data saved successfully.");
   }
   process.exit();
+}
+
+function toValidKey(keyString) {
+  return keyString.replace(new RegExp("[.#$/\\]\\[]", "g"), "");
+}
+
+function pad(n, width = 2, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
